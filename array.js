cfacd980df77c305f00083d8cb87fc868e75c442
Riley Palmer <riley@mail.boolean44.is-a.dev>
2022-05-03 22:45:47 -0400

    init

---
+++ array.js
@@ -0,0 +1,18 @@
+module.exports = [
+    "coon",
+    "c00n",
+    "fag",
+    "f@g",
+    "nigger",
+    "n1gger",
+    "nigg3r",
+    "n1gg3r",
+    "kike",
+    "k1ke",
+    "kik3",
+    "k1k3",
+    "tranny",
+    "tr@nny",
+    "retard",
+    "r3tard",
+]
